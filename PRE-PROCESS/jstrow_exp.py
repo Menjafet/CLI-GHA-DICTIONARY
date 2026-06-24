c20608a6df7ffe9c33a922d2fd32132d5308e628
Jafet Rodriguez Mendez <42688559+Menjafet@users.noreply.github.com>
2026-06-23 23:40:17 -0600

    ARAMAIC DICT INCLUDED AND FODLER RE-ARANGEMENT

---
+++ PRE-PROCESS/jstrow_exp.py
@@ -0,0 +1,38 @@
+import json
+
+INPUT_FILE='./dicts/dictfull.json'
+OUTPUT_FILE='./dicts/dictfull.tsv'
+#giveup dialectics but
+def convert_json_tsv():
+    try:
+        with open(file=INPUT_FILE,mode="r" ,encoding='utf-8') as file,\
+            open(file=OUTPUT_FILE,mode="w" ,encoding='utf-8')   as outfile:
+                
+            data=(json.load(file)).items()
+            
+            for id,(word,entry) in enumerate(  data,start=1):
+                def_segments:list[str] =[]
+                entry_basic=f"{id}\t{word}"
+                if(id==7):print("asd")
+
+                for (version, v_data) in entry.items():
+                    def_segments.append(f"[@{version}] ")    
+                    for contents in v_data:
+                        def_segments.append(f"{contents['def_num']}) {contents['definition']}")
+            
+                # if(id==72): break#it is for making demos not the entire json
+                full_t=" ".join(def_segments)
+                clean_definition= " ".join(full_t.split())  
+
+                outfile.write(f"{id}\t{word}\t{clean_definition}\n")
+            
+            print("success")
+            
+
+    except FileNotFoundError:
+        print('file not found')
+    except json.JSONDecodeError:    
+        print('error parsing json')
+
+if __name__ == "__main__":
+    convert_json_tsv()
--- main.c
+++ main.c
@@ -3,28 +3,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-uint32_t
-swap_uint32 (uint32_t v)
+typedef char bool;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef struct
 {
+  const u32 lines;
+  const char *idx;
+  const char *tsv;
+} Lang;
+
+u32
+swap_u32 (uint32_t v)
+{ // 4 bytes 32 bits 1byte 4times 8+8+8+8
+  // from little indian to big indian
   return ((v << 24) & 0xff000000) | ((v << 8) & 0x00ff0000)
          | ((v >> 8) & 0x0000ff00) | ((v >> 24) & 0x000000ff);
 }
 
 // GENERATE IDX FILES
 void
-PRINT_LINE_SIZES_AND_POSITIONS (FILE *file_pointer, FILE *idx_file)
+PRINT_LINE_SIZES_AND_POSITIONS (Lang language, FILE *file_pointer,
+                                FILE *idx_file)
 {
   //  printf ("sda\n");
-  uint32_t header[3]
-      = { swap_uint32 (0x00000C02), // magic number signed 32bit int,2d
-          swap_uint32 (8675), swap_uint32 (2) };
+  u32 header[3] = { swap_u32 (0x00000C02), // magic number to validate it is
+                                           // indeed your dicitonary index file
+                    swap_u32 (language.lines), swap_u32 (2) };
 
-  fwrite (header, sizeof (uint32_t), 3, idx_file);
+  fwrite (header, sizeof (u32), 3, idx_file);
   fseek (file_pointer, 0, SEEK_SET);
-  uint32_t str_e = 0;
-  uint32_t end_e = 0;
+  u32 str_e = 0;
+  u32 end_e = 0;
 
-  int c;
+  i64 c;
   while ((c = fgetc (file_pointer)) != EOF)
     {
 
@@ -33,13 +52,13 @@ PRINT_LINE_SIZES_AND_POSITIONS (FILE *file_pointer, FILE *idx_file)
           // get exact byte position
           end_e = ftell (file_pointer);
           // end position-start position -1\n
-          uint32_t real_size = end_e - str_e - 1;
+          u32 real_size = end_e - str_e - 1;
 
-          uint32_t pos = swap_uint32 (str_e);
-          uint32_t siz = swap_uint32 (real_size);
+          u32 pos = swap_u32 (str_e);
+          u32 siz = swap_u32 (real_size);
 
-          fwrite (&pos, sizeof (uint32_t), 1, idx_file);
-          fwrite (&siz, sizeof (uint32_t), 1, idx_file);
+          fwrite (&pos, sizeof (u32), 1, idx_file);
+          fwrite (&siz, sizeof (u32), 1, idx_file);
 
           str_e = end_e;
         }
@@ -50,7 +69,8 @@ void
 provide_entry (FILE *file_pointer, FILE *idx_file, int entry_index)
 { //  int ret_code = 0
   // 12 bytes header + index * 2elemts per row* 4bytes per elemt
-  long idx_offset = 12 + (entry_index * 2 * sizeof (uint32_t));
+  // long idx_offset = 12 + (entry_index * 2 * sizeof (u32));
+  long idx_offset = 12 + (entry_index * 2 * sizeof (u32));
 
   if (fseek (idx_file, idx_offset, SEEK_SET) != 0)
     {
@@ -62,12 +82,12 @@ provide_entry (FILE *file_pointer, FILE *idx_file, int entry_index)
       exit (EXIT_FAILURE);
     }
 
-  uint32_t raw_position, raw_size;
-  fread (&raw_position, sizeof (uint32_t), 1, idx_file);
-  fread (&raw_size, sizeof (uint32_t), 1, idx_file);
+  u32 raw_position, raw_size;
+  fread (&raw_position, sizeof (u32), 1, idx_file);
+  fread (&raw_size, sizeof (u32), 1, idx_file);
   // fseek
-  uint32_t real_size = swap_uint32 (raw_size);
-  uint32_t real_position = swap_uint32 (raw_position);
+  u32 real_size = swap_u32 (raw_size);
+  u32 real_position = swap_u32 (raw_position);
   if (fseek (file_pointer, real_position, SEEK_SET) != 0)
     {
 
@@ -78,7 +98,7 @@ provide_entry (FILE *file_pointer, FILE *idx_file, int entry_index)
       exit (EXIT_FAILURE);
     }
 
-  //printf ("real size %d", real_size);
+  // printf ("real size %d", real_size);
   char *buffer = (char *)malloc (real_size + 1);
   fread (buffer, sizeof (char), real_size, file_pointer);
   buffer[real_size] = '\0';
@@ -90,15 +110,50 @@ typedef enum
   N, // NOT FOUND
   G, // GREEK
   H, // HEBREW
+  A, // ARAMAIC
   R  // RE INDEX DICTIONARIES
 } Command;
+
+typedef struct
+{
+  Command type;
+  union
+  {
+    char str[234];
+    char byte;
+    int number;
+  };
+} languino;
+
+void
+polimorph_print (languino *lan)
+{
+  switch (lan->type)
+    {
+    case N:
+      printf ("%d", lan->number);
+    case R:
+    case A:
+
+      printf ("%c", lan->byte);
+    case G:
+      printf ("%c", lan->byte);
+      break;
+    case H:
+      printf ("%s", lan->str);
+      break;
+    }
+}
 
 Command
 getCommand (char *val)
 {
+  // reminder= strcmp 0 means equal
   assert (!(val == NULL));
   if (0 == strcmp (val, "-h"))
     return H;
+  else if (0 == strcmp (val, "-a"))
+    return A;
   else if (0 == strcmp (val, "-g"))
     return G;
   else if (0 == strcmp (val, "-r"))
@@ -107,22 +162,31 @@ getCommand (char *val)
   return N;
 }
 
-typedef struct
+bool
+is_arg_val_number (char *arg_val)
 {
-  const char *idx;
-  const char *tsv;
-} Lang;
+  if (45 <= arg_val[0] && arg_val[0] <= 57)
+    return 'a';
+  return ' ';
+}
 
 int
 main (int argc, char *argv[])
 {
-  const Lang HEBREW
-      = { .idx = "hebrew_strong.idx", .tsv = "strongHebrewG_c.tsv" };
-  const Lang GREEK
-      = { .idx = "greek_strong.idx", .tsv = "strongsgreek_c.tsv" };
-  // main -g entry
-  // main -h entry
-  // main -r
+  const Lang ARAMAIC = { .lines = 8215,
+                         .idx = "dictfull.idx",
+                         .tsv = "./dicts/dictfull.tsv" };
+  const Lang HEBREW = { .lines = 8676,
+                        .idx = "hebrew_strong.idx",
+                        .tsv = "strongHebrewG_c.tsv" };
+  const Lang GREEK = { .lines = 5624,
+                       .idx = "greek_strong.idx",
+                       .tsv = "strongsgreek_c.tsv" };
+  char *help = "commands available \n gha -g entry ->greek entry of strong "
+               "dicitonary\n gha -h entry ->strong hebrew entry\n gha -a "
+               "entry -> entry in aramaic\n gha -r reindex all the "
+               "dictionaries from the  tsv's"
+               "\n gha -s word ->search exact match";
 
   int entry = 0;
   if (argc >= 2)
@@ -132,57 +196,84 @@ main (int argc, char *argv[])
 
       FILE *idx_file_G = fopen (GREEK.idx, "r+");
       FILE *idx_file_H = fopen (HEBREW.idx, "r+");
+      FILE *file_pointer_AR = fopen (ARAMAIC.tsv, "r");
+      FILE *idx_file_AR = fopen (ARAMAIC.idx, "r+");
 
       // set error guardrails
+      assert (!(idx_file_AR == NULL));
+      assert (!(file_pointer_AR == NULL));
       assert (!(idx_file_G == NULL));
       assert (!(idx_file_H == NULL));
       assert (!(file_pointer_H == NULL));
       assert (!(file_pointer_G == NULL));
       if (getCommand (argv[1]) == R)
         {
+          // lets get the aramaic in
 
-          PRINT_LINE_SIZES_AND_POSITIONS (file_pointer_G, idx_file_G);
-          PRINT_LINE_SIZES_AND_POSITIONS (file_pointer_H, idx_file_H);
+          PRINT_LINE_SIZES_AND_POSITIONS (ARAMAIC, file_pointer_AR,
+                                          idx_file_AR);
+          PRINT_LINE_SIZES_AND_POSITIONS (GREEK, file_pointer_G, idx_file_G);
+          PRINT_LINE_SIZES_AND_POSITIONS (HEBREW, file_pointer_H, idx_file_H);
+          fclose (file_pointer_AR);
           fclose (file_pointer_H);
           fclose (file_pointer_G);
+          fclose (idx_file_AR);
           fclose (idx_file_H);
           fclose (idx_file_G);
-          return 0;
+          printf ("reindexing successful!");
+          int GREAT_SUCCESS = 0;
+          return GREAT_SUCCESS;
         }
       // validate it is a number
       if (NULL == argv[2])
         {
-          printf ("entry not provided: ");
+          printf ("argument not provided:see -> ");
+          printf ("%s", help);
           exit (1);
         }
-      char *s = argv[2];
 
-      entry = atoi (s);
-      switch (getCommand (argv[1]))
+      char *arg_val = argv[2];
+      if (is_arg_val_number (arg_val) == 'a')
         {
-        case H:
-          provide_entry (file_pointer_H, idx_file_H, entry);
-          break;
-        case G:
-          provide_entry (file_pointer_G, idx_file_G, entry);
-          break;
-        case R:
-        case N:
+          entry = atoi (arg_val);
+          switch (getCommand (argv[1]))
+            {
+            case H:
+              provide_entry (file_pointer_H, idx_file_H, entry);
+              break;
+            case G:
+              provide_entry (file_pointer_G, idx_file_G, entry);
+              break;
+            case A:
+              provide_entry (file_pointer_AR, idx_file_AR, entry);
+              break;
+            case R:
+            case N:
 
-          fclose (file_pointer_H);
-          fclose (file_pointer_G);
-          fclose (idx_file_H);
-          fclose (idx_file_G);
-          perror ("UNKNOWN COMMAND");
-          return 1;
-          break;
+              fclose (file_pointer_H);
+              fclose (file_pointer_G);
+              fclose (idx_file_H);
+              fclose (idx_file_G);
+              perror ("UNKNOWN COMMAND");
+              printf (":see ->%s ", help);
+              return 1;
+              break;
+            }
         }
-
+      else
+        {
+          printf ("so far is not a number");
+        }
+      fclose (file_pointer_AR);
       fclose (file_pointer_H);
       fclose (file_pointer_G);
+      fclose (idx_file_AR);
       fclose (idx_file_H);
       fclose (idx_file_G);
+      return 0;
     }
 
+  printf ("Command not provided:see -> ");
+  printf ("%s", help);
   return 0;
 }
